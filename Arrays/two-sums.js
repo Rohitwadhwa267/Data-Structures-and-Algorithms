4275da72f205c440ddeb70063d9bb46cf62b9885
Rohit wadhwa <rohitwadhwa267@gmail.com>
2025-01-13 22:31:36 +0530

    Add two-sum solution with example usage and formatting improvements

--- Arrays/two-sums.js
+++ Arrays/two-sums.js
@@ -8,11 +8,12 @@ class Solution {
         const indices = {};  // val -> index
 
         for (let i = 0; i < nums.length; i++) {
-            indices[nums[i]] = i;
+            indices[nums[i]] = i;            
         }
 
+
         for (let i = 0; i < nums.length; i++) {
-            let diff = target - nums[i];
+            let diff = target - nums[i];            
             if (indices[diff] !== undefined && indices[diff] !== i) {
                 return [i, indices[diff]];
             }
@@ -21,3 +22,7 @@ class Solution {
         return [];
     }
 }
+
+const solution = new Solution();
+console.log(solution.twoSum([2, 17, 11, 7], 9));  // [0, 1]
+// console.log(solution.twoSum([3, 2, 4], 6));  // [1, 2]
