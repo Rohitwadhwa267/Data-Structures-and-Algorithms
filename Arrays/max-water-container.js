71d4ea8c12391ded32a33e7153d1742cc7d15e2a
Rohit wadhwa <rohitwadhwa267@gmail.com>
2025-01-31 23:14:10 +0530

    Add solution for maximum water container problem using two pointers

--- Arrays/max-water-container.js
+++ Arrays/max-water-container.js
@@ -17,3 +17,29 @@ class Solution {
 const solution = new Solution();
 console.log(solution.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));  // 49
 // console.log(solution.maxArea([1, 1]));  // 1
+
+
+
+// By using two pointers, we can solve this problem in O(n) time complexity.
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let l = 0;
+        let r = heights.length - 1;
+        let res = 0;
+
+        while (l < r) {
+            const area = Math.min(heights[l], heights[r]) * (r - l);
+            res = Math.max(res, area);
+            if (heights[l] <= heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+}
