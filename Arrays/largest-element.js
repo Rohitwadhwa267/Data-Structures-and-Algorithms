a7b9f7b0b56faf557d79eecc7c4edb7a8f1df896
Rohit wadhwa <rohitwadhwa267@gmail.com>
2025-02-14 23:35:35 +0530

    Add function to find the largest element in an array

--- Arrays/largest-element.js
+++ Arrays/largest-element.js
@@ -1,11 +1,20 @@
 
+/**
+ * Function to find the largest element in an array
+ * @param {Array} arr The array to search through
+ * @returns {Number} The largest element in the array
+ */
 function findLargestElement(arr) {
+    // start with the first element as the current max
     let max = arr[0];
+    // loop through the array starting with the second element
     for (let i = 0; i < arr.length; i++) {
+        // if the current element is greater than the current max, update max
         if (max < arr[i]) {
             max = arr[i];
         }
     }
+    // return the max element
     return max;
 }
 
